083bb75d14a6a9a01da2c0423bc5caee9e9073b1
a.melnikau <a.melnikau@gmail.com>
2022-10-22 02:19:05 +0300

    add task 7 better solution

---
+++ Homeworks1022/task7-1.py
@@ -0,0 +1,17 @@
+"""Дан список чисел.
+Если в нем есть два соседних элемента одного знака, выведите эти числа.
+Если соседних элементов одного знака нет - не выводите ничего.
+Если таких пар соседей несколько - выведите первую пару. """
+import math
+
+stack = [2, -4, 11, 0, -67, 0, 5, 45, -9, -45, 5, -7, 0, -1, -3]
+i = 0
+list_pairs = []
+
+while i < len(stack) - 1:
+    if (stack[i] > 0 and stack[i + 1] > 0) or (stack[i] < 0 and stack[i + 1] < 0):
+        print(stack[i], stack[i + 1])  # returns all pairs
+        list_pairs.append([stack[i], stack[i + 1]])
+    i += 1
+
+print(list_pairs[:1])  # returns 1st pair
