79ae24db624ee2a3d3adaddfd351a359a0bf9c9e
a.melnikau <a.melnikau@gmail.com>
2022-10-30 15:38:26 +0300

    add classwork1029

---
+++ test1026.py
@@ -0,0 +1,16 @@
+from Homeworks1022.task1 import stack
+from Homeworks1022.task2 import stack
+
+#
+# arr = list(map(int, input().split()))
+#
+# i = 0
+# rostPeti = 180
+#
+# while i < len(arr):
+#     if arr[i] > rostPeti:
+#         print(i+1)
+#         break
+#     i += 1
+
+print(stack)
